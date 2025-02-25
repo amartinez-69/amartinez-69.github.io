fa3aeed91325595299bbb4181b38f037dd08a590
amartinez-69 <amartinez69@smith.edu>
2025-02-24 22:08:06 -0500

    4

--- CSC209/Hwks/Hwk4/technical/js/technical.js
+++ CSC209/Hwks/Hwk4/technical/js/technical.js
@@ -2,16 +2,11 @@ function startMovement(){
     const squares = document.querySelectorAll(".squares");
     const squareData = [];
 
-    squares.forEach((square, index) => {
-        // Initial direction of motion: positive for moving down/right, negative for up/left
-        let direction = index % 2 === 0 ? 1 : -1; // alternate direction for variety
-        
+    squares.forEach((square) => {
         squareData.push({
             square,
             topPos: square.top,
             leftPos: square.left,
-            topDirection: direction, // 1 or -1
-            leftDirection: direction  // 1 or -1
         });
     });
     console.log(squareData);
