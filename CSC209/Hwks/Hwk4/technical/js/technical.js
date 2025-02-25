781ce2e1af483205eabbbb7c7034bd1b093d9c96
amartinez-69 <amartinez69@smith.edu>
2025-02-24 22:03:21 -0500

    2

--- CSC209/Hwks/Hwk4/technical/js/technical.js
+++ CSC209/Hwks/Hwk4/technical/js/technical.js
@@ -3,18 +3,13 @@ function startMovement(){
     const squareData = [];
 
     squares.forEach((square, index) => {
-        let initialPosition = {
-            top: parseInt(square.style.top),
-            left: parseInt(square.style.left)
-        };
-
         // Initial direction of motion: positive for moving down/right, negative for up/left
         let direction = index % 2 === 0 ? 1 : -1; // alternate direction for variety
         
         squareData.push({
             square,
-            topPos: initialPosition.top,
-            leftPos: initialPosition.left,
+            topPos: square.top,
+            leftPos: square.left,
             topDirection: direction, // 1 or -1
             leftDirection: direction  // 1 or -1
         });
