cbd04506bc58a6f7284b544bff01074248ae8def
amartinez-69 <amartinez69@smith.edu>
2025-02-25 22:05:01 -0500

    9

--- CSC209/Hwks/Hwk4/Hwk3Redone/technical/javascript/javascript.js
+++ CSC209/Hwks/Hwk4/Hwk3Redone/technical/javascript/javascript.js
@@ -1,4 +1,39 @@
 let slideIndex = 1;
+
+let images = ["files/image1.jpeg", "files/image2.webp", "files/triumphant-success-reaching-new-heights-of-achievement-ai-generative-photo.jpg"];
+
+let captions = ['This is me choosing the images for this assignment.', 'This is me eating dinner while doing this assignment.', 'This will be me when I finish this assignment'];
+
+function loadSlides() {
+    let slideshowContainer = document.getElementById("slideshow");
+
+    for (let i = 0; i < images.length; i++) {
+        let slideDiv = document.createElement("div");
+        slideDiv.className = "mySlides";
+
+        let slideNum = document.createElement("div");
+        slideNum.className = 'numberText';
+        slideNum.textContent = `${i + 1} / ${images.length}`;
+        slideNum.style.marginLeft = "-170%";  // Adjust as needed
+        slideNum.style.marginTop = "-72%"; 
+        slideNum.style.color = 'white';
+        slideNum.style.fontWeight = 'bold';
+
+        let img = document.createElement("img");
+        img.src = images[i];
+        img.style = "width:100%"
+        img.alt = "Slide " + (i + 1);
+
+        let caption = document.createElement("div");
+        caption.className = 'text';
+        caption.textContent = captions[i];
+
+        slideDiv.appendChild(caption);
+        slideDiv.appendChild(img);
+        slideDiv.appendChild(slideNum);
+        slideshowContainer.appendChild(slideDiv);
+    }
+  }
 showSlides(slideIndex);
 
 function plusSlides(n) {
