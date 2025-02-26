afc731aef82d55b7f5baf70090dfc3000313ca59
amartinez-69 <amartinez69@smith.edu>
2025-02-25 20:41:16 -0500

    3

--- CSC209/Hwks/Hwk4/technical/js/technical.js
+++ CSC209/Hwks/Hwk4/technical/js/technical.js
@@ -2,12 +2,5 @@ function startMovement(){
     const squares = document.getElementsByClassName("squares");
     const squareData = [];
 
-    squares.forEach((square) => {
-        squareData.push({
-            square,
-            topPos: square.style.top,
-            leftPos: square.style.left,
-        });
-    });
     console.log(squares);
 }
