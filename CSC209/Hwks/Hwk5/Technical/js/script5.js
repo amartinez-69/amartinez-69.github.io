5ffada897c903fb09dd888378e57bb88050256c0
amartinez-69 <amartinez69@smith.edu>
2025-03-03 00:14:01 -0500

    7

--- CSC209/Hwks/Hwk5/Technical/js/script5.js
+++ CSC209/Hwks/Hwk5/Technical/js/script5.js
@@ -42,8 +42,8 @@ function drawVector(point) {
 function updatePoints() {
     for(i = 0; i < points.length; i++){
         // Scale the vector by the speed factor
-        points[i].x += points[i].vector.x * NRSTEPS * 0.005;
-        points[i].y += points[i].vector.y * NRSTEPS * 0.005;
+        points[i].x += points[i].vector.x * NRSTEPS * 0.05;
+        points[i].y += points[i].vector.y * NRSTEPS * 0.05;
     };
 }
 
@@ -57,15 +57,15 @@ function drawScene() {
     }
 }
         // Animation loop
-const intervalTime = 50;
-setInterval(() => {
-    drawScene();
-    updatePoints();
-    }, intervalTime); 
 
 // Update points when button is clicked
 button.addEventListener('click', () => {
     drawScene();
+    const intervalTime = 50;
+    setInterval(() => {
+        drawScene();
+        updatePoints();
+    }, intervalTime); 
 }); 
 
 ;
