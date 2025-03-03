8e4b8cb3f9118bae7b39cf4dfd31bd1b49ca55e4
amartinez-69 <amartinez69@smith.edu>
2025-03-02 19:30:08 -0500

    1

--- CSC209/Hwks/Hwk5/Technical/js/script.js
+++ CSC209/Hwks/Hwk5/Technical/js/script.js
@@ -2,13 +2,18 @@ const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
 
 // Point properties
-const point = { x: 150, y: 150, radius: 10, color: 'blue' };
+//const point = { x: 150, y: 150, radius: 10, color: 'blue' };
 
 // Vector properties
-const vector = { x: 15, y: 15, color: 'blue' };
+//const vector = { x: 15, y: 15, color: 'blue' };
 
+const points = [
+    { x: 150, y: 150, radius: 10, color: 'red', vector: { x: 15, y: 15 } },
+    { x: 125, y: 125, radius: 10, color: 'green', vector: { x: -15, y: 15 } },
+    { x: 200, y: 100, radius: 10, color: 'blue', vector: { x: 15, y: -15 } }
+];
 // Draw the point
-function drawPoint() {
+function drawPoint(point) {
     ctx.beginPath();
     ctx.arc(point.x, point.y, point.radius, 0, Math.PI * 2);
     ctx.strokeStyle = point.color;
@@ -16,10 +21,10 @@ function drawPoint() {
 }
 
 // Draw the vector
-function drawVector() {
+function drawVector(point) {
     ctx.beginPath();
     ctx.moveTo(point.x, point.y);
-    ctx.lineTo(point.x + vector.x, point.y + vector.y);
+    ctx.lineTo(point.x + point.vector.x, point.y + point.vector.y);
     ctx.strokeStyle = vector.color;
     ctx.lineWidth = 2;
     ctx.stroke();
@@ -28,8 +33,10 @@ function drawVector() {
 // Draw the scene
 function drawScene() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawPoint();
-    drawVector();
+    for(let i = 0; i < points.length; i++){
+        drawPoint(points[i]);
+        drawVector(points[i]);
+    }
 }
 
 drawScene();  // Initial draw
