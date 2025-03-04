9239f0ba9d415f1c193e8e6d98ab196e85c61e92
amartinez-69 <amartinez69@smith.edu>
2025-03-03 23:43:24 -0500

    2

--- CSC209/Hwks/Hwk5/Technical/js/script9.js
+++ CSC209/Hwks/Hwk5/Technical/js/script9.js
@@ -61,7 +61,7 @@ function updatePoints() {
         // Scale the vector by the speed factor
         points[i].x += points[i].vector.x * 0.5;
         points[i].y += points[i].vector.y * 0.5;
-        points[i].trail.push({ x: points[i].x, y: points[i].y });
+        trail.push({ x: points[i].x, y: points[i].y });
 
         if (points[i].x <= 0 || points[i].x >= canvas.width) {
             points[i].vector.x = -points[i].vector.x; // Reverse x velocity
