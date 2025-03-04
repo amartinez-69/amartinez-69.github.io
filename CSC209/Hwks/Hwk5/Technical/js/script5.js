e21a4f4db9a0cfdfce2b67d3ec44cde5d128eb6e
amartinez-69 <amartinez69@smith.edu>
2025-03-03 19:09:32 -0500

    3

--- CSC209/Hwks/Hwk5/Technical/js/script5.js
+++ CSC209/Hwks/Hwk5/Technical/js/script5.js
@@ -8,7 +8,7 @@ const button = document.getElementById("generateNewLocations");
 // Vector properties
 //const vector = { x: 15, y: 15, color: 'blue' };
 
-const NRSTEPS = 50;
+const NRSTEPS = 10;
 
 const points = [
     { x: 0, y: 0, radius: 10, color: 'red', vector: { x: 15, y: 15 } },
