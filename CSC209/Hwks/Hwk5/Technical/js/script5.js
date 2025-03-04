0ade4fa181fda0601040f46b77c788ec92179300
amartinez-69 <amartinez69@smith.edu>
2025-03-03 19:26:23 -0500

    9

--- CSC209/Hwks/Hwk5/Technical/js/script5.js
+++ CSC209/Hwks/Hwk5/Technical/js/script5.js
@@ -44,13 +44,13 @@ function updatePoints() {
         // Scale the vector by the speed factor
         points[i].x += points[i].vector.x * NRSTEPS;
         points[i].y += points[i].vector.y * NRSTEPS;
-        if (point[i].x <= 0 || point[i].x >= canvas.width) {
+        /*if (point[i].x <= 0 || point[i].x >= canvas.width) {
             point[i].vector.x = -point[i].vector.x; // Reverse x velocity
         }
 
         if (point[i].y <= 0 || point[i].y >= canvas.height) {
             point[i].vector.y = -point[i].vector.y; // Reverse y velocity
-        }
+        }*/
     }
 }
 
