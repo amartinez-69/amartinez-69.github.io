3f792a68dd21f2062765a16b4f9defabd3fa79f1
amartinez-69 <amartinez69@smith.edu>
2025-03-10 17:42:00 -0400

    8

--- CSC209/Hwks/Hwk6/technical/js/script.js
+++ CSC209/Hwks/Hwk6/technical/js/script.js
@@ -41,7 +41,7 @@
         }
 
         drawGrid(){
-            this.ctx.learRect(0, 0, this.canvasId.width, this.canvas.height);
+            this.ctx.clearRect(0, 0, this.canvasId.width, this.canvas.height);
 
             for(let row = 0; row < this.rows; row++){
                 for(let col = 0; col < this.cols; col++){
