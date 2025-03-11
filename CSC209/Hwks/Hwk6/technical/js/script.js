5196f34b8acfeefd286ee7dd4f255bc0f9de647b
amartinez-69 <amartinez69@smith.edu>
2025-03-10 21:54:05 -0400

    1

--- CSC209/Hwks/Hwk6/technical/js/script.js
+++ CSC209/Hwks/Hwk6/technical/js/script.js
@@ -1,6 +1,6 @@
 (function() {
     class GameGrid {
-
+        //contructor for GameGrid class
         constructor(canvasId, rows, cols, cell) {
             this.canvasId = canvasId;
             this.rows = rows;
@@ -9,16 +9,16 @@
         }
 
         init() {
-
+            //assigns all parameters for the grid
             this.canvas = document.getElementById(this.canvasId);
             this.ctx = this.canvas.getContext("2d");
             this.canvas.width = this.cols * this.cell;
             this.canvas.height = this.rows * this.cell; 
             this.grid = Array.from({length: this.rows}, () => Array(this.cols).fill(0)); 
-
+            //draws grid and initializes cell pattern
             this.drawGrid();
             this.setPattern();
-
+            //next button 
             document.getElementById('next').addEventListener("click", () => {
                 gameGrid.updateGrid(); 
             });
@@ -27,10 +27,11 @@
         }
 
         setPattern(){
-            this.grid = Array.from({length: this.rows}, () => Array(this.cols).fill(0));
+            this.grid = Array.from({length: this.rows}, () => Array(this.cols).fill(0)); //array method to make 2d array
+            //start cell
             let startRow = 6;
             let startCol = 6;
-
+            //makes pattern
             this.grid[startRow][startCol] = 1; 
             this.grid[startRow][startCol + 1] = 1;
             this.grid[startRow][startCol + 2] = 1;
@@ -47,17 +48,17 @@
                 for(let col = 0; col < this.cols; col++){
                     this.ctx.fillStyle = this.grid[row][col] ? "black" : "white";
                     this.ctx.fillRect(col * this.cell, row * this.cell, this.cell, this.cell);
-                    this.ctx.strokeStyle = "gray";
+                    this.ctx.strokeStyle = "black";
                     this.ctx.strokeRect(col * this.cell, row * this.cell, this.cell, this.cell);
                 }
             }
         }
-
+        //Generates next grid to draw 
         nextGen() {
             let newGrid = Array.from({ length: this.rows }, () => Array(this.cols).fill(0));
             for(let row = 0; row < this.rows; row++){
                 for(let col = 0; col < this.cols; col++){
-                    let numNeighbors = this.liveNeighbors(row, col);
+                    let numNeighbors = this.liveNeighbors(row, col); //liveNeighbors checks how many live neighbors (colored cells) the cell has. if it has more than 2 it dies
                     if(this.grid[row][col] === 1){
                         newGrid[row][col] = (numNeighbors === 2 || numNeighbors === 3) ? 1 : 0;
                     } else {
@@ -68,7 +69,7 @@
             }
             return newGrid;
         }
-
+        //Look one comment above
         liveNeighbors(row, col){
             let directions = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]];
             let count = 0;
